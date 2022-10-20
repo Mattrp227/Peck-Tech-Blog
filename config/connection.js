752fef0311521cacd95063c1e7e551f8ce984f70
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-20 00:36:48 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,17 @@
 
+const Sequelize = require("sequelize");
+
+let sequelize;
+
+ord
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+  );
+}
+
+module.exports = sequelize;
