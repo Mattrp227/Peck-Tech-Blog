2bfcf4c4fa15b86f6a506a7c1449ee1cb507aabd
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-20 00:34:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
+//create constructors 
+const path = require('path');
+const express = require('express');
+const exsess = require('express-session');
+const handles = require('express-handlebars');
+
+
+const routes = require('./routes');
+const sequel = require('./config/connection');
+const helpe = require('./utils/helpers');
+
+
+
+const sess = {
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+};
+
+
+// set up handlebars.js engine instance with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// set which template engine to use.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
 
