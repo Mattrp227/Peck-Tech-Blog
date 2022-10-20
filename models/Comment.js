dca582accf97d42fa7ddf3da810fd268c55e88df
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-20 00:39:16 -0400

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,40 @@
+//constructors
+const { Model, DataTypes } = require('sequelize');
+const Comment;
 
+
+
+
+// define table columns and configuration
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
